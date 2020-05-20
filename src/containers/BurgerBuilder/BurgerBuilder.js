897a3a7270e4cce9169bd249f2b31c2060f5748a
Davi Viana <davipviana@gmail.com>
2020-05-20 15:36:54 -0300

    burger builder component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Wrapper from '../../hoc/Wrapper';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Wrapper>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Wrapper>
+        );
+    };
+}
+
+export default BurgerBuilder;
