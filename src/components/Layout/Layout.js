5046a536d43b36cfaf2abbcc1bb0ccf32bed591d
Davi Viana <davipviana@gmail.com>
2020-05-20 15:37:00 -0300

    layout margin

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Wrapper from '../../hoc/Wrapper';
+import styles from './Layout.module.css';
 
 const Layout = (props) => (
     <Wrapper>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={styles.Content}>
             {props.children}
         </main>
     </Wrapper>
