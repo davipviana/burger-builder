df082543b87e8640898dba4afcc94cb9170371bb
Davi Viana <davipviana@gmail.com>
2020-05-20 17:07:43 -0300

    render burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,15 +3,28 @@ import React from 'react';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import styles from './Burger.module.css';
 
-const Burger = (props) => {
+const Burger = ({ ingredients }) => {
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {getBurgerIngredients(ingredients)}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
+}
+
+const getBurgerIngredients = (ingredients) => {
+    const ingredientsTypes = Object.keys(ingredients);
+
+    const burgerIngredients = ingredientsTypes.map(ingredientType => {
+        const ingredientCount = ingredients[ingredientType];
+
+        return [...Array(ingredientCount)].map((_, index) => (
+            <BurgerIngredient key={ingredientType + index} type={ingredientType} />
+        ))
+    });
+
+    return burgerIngredients;
 }
 
 export default Burger;
