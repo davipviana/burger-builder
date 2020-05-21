b820674b67c6cdd9e5e4008c74459247ed011332
Davi Viana <davipviana@gmail.com>
2020-05-21 14:00:42 -0300

    open and close side drawer actions

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Wrapper from '../../hoc/Wrapper';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
@@ -6,15 +6,29 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import styles from './Layout.module.css';
 
-const Layout = (props) => (
-    <Wrapper>
-        <Toolbar />
-        <SideDrawer />
-        <div>SideDrawer, Backdrop</div>
-        <main className={styles.Content}>
-            {props.children}
-        </main>
-    </Wrapper>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    };
+
+    hideSideDrawerHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    render() {
+        return (
+            <Wrapper>
+                <Toolbar />
+                <SideDrawer
+                    visible={this.state.showSideDrawer}
+                    onClose={this.hideSideDrawerHandler} />
+                <div>SideDrawer, Backdrop</div>
+                <main className={styles.Content}>
+                    {this.props.children}
+                </main>
+            </Wrapper>
+        );
+    }
+}
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -8,11 +8,16 @@ import Wrapper from '../../../hoc/Wrapper';
 import styles from './SideDrawer.module.css';
 
 const SideDrawer = (props) => {
+    let sideDrawerClasses = [styles.SideDrawer, styles.Closed];
+
+    if (props.visible) {
+        sideDrawerClasses = [styles.SideDrawer, styles.Open];
+    }
 
     return (
         <Wrapper>
-            <Backdrop visible />
-            <div className={styles.SideDrawer}>
+            <Backdrop visible={props.visible} onClick={props.onClose} />
+            <div className={sideDrawerClasses.join(' ')}>
                 <div className={styles.Logo}>
                     <Logo />
                 </div>
