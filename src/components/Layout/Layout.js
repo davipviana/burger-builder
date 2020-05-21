26a9e18e3a9e2e977560954f305029594bc0099e
Davi Viana <davipviana@gmail.com>
2020-05-21 11:40:29 -0300

    toolbar component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import Wrapper from '../../hoc/Wrapper';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 import styles from './Layout.module.css';
 
 const Layout = (props) => (
     <Wrapper>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
+        <div>SideDrawer, Backdrop</div>
         <main className={styles.Content}>
             {props.children}
         </main>
