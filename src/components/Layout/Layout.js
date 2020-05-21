c8df3edcd1c8738a20c20dd0c6e650235939c861
Davi Viana <davipviana@gmail.com>
2020-05-21 12:23:29 -0300

    side drawer component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Wrapper from '../../hoc/Wrapper';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import styles from './Layout.module.css';
 
 const Layout = (props) => (
     <Wrapper>
         <Toolbar />
+        <SideDrawer />
         <div>SideDrawer, Backdrop</div>
         <main className={styles.Content}>
             {props.children}
