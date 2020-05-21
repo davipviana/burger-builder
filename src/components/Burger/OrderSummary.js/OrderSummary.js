d68c6dc9eb706c3c263bb8ddee9ade635aa2d89e
Davi Viana <davipviana@gmail.com>
2020-05-21 18:20:20 -0300

    check if modal should update to improve performance

--- src/components/Burger/OrderSummary.js/OrderSummary.js
+++ src/components/Burger/OrderSummary.js/OrderSummary.js
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Wrapper from '../../../hoc/Wrapper';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props) => {
-    const ingredientsTypes = Object.keys(props.ingredients);
-    const ingredientSummary = ingredientsTypes
-        .map(ingredientType => {
-            const ingredientCount = props.ingredients[ingredientType];
-            return (
-                <li key={ingredientType}>
-                    <span style={{ textTransform: 'capitalize' }}>{ingredientType}</span>: {ingredientCount}
-                </li>
-            );
-        });
+class OrderSummary extends Component {
+    render() {
+        const ingredientsTypes = Object.keys(this.props.ingredients);
+        const ingredientSummary = ingredientsTypes
+            .map(ingredientType => {
+                const ingredientCount = this.props.ingredients[ingredientType];
+                return (
+                    <li key={ingredientType}>
+                        <span style={{ textTransform: 'capitalize' }}>{ingredientType}</span>: {ingredientCount}
+                    </li>
+                );
+            });
 
-    return (
-        <Wrapper>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button type="Danger" onClick={props.onCancel}>CANCEL</Button>
-            <Button type="Success" onClick={props.onContinue}>CONTINUE</Button>
-        </Wrapper>
-    );
+        return (
+            <Wrapper>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button type="Danger" onClick={this.props.onCancel}>CANCEL</Button>
+                <Button type="Success" onClick={this.props.onContinue}>CONTINUE</Button>
+            </Wrapper>
+        );
+    }
 };
 
 export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Wrapper from '../../../hoc/Wrapper';
 import Backdrop from '../Backdrop/Backdrop';
 
 import styles from './Modal.module.css';
 
-const Modal = (props) => (
-    <Wrapper>
-        <Backdrop visible={props.visible} onClick={props.onModalClosed} />
-        <div
-            className={styles.Modal}
-            style={{
-                transform: props.visible ? 'translateY(0)' : 'translateY(-100vh)'
-            }}>
-            {props.children}
-        </div>
-    </Wrapper>
-);
+class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, _) {
+        return (nextProps.visible !== this.props.visible);
+    }
+
+    render() {
+        return (
+            <Wrapper>
+                <Backdrop visible={this.props.visible} onClick={this.props.onModalClosed} />
+                <div
+                    className={styles.Modal}
+                    style={{
+                        transform: this.props.visible ? 'translateY(0)' : 'translateY(-100vh)'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Wrapper>
+        );
+    }
+}
 
 export default Modal;
