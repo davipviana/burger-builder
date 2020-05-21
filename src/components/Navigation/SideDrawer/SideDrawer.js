1619e370d72b3160764b13d84b3f0ee2f893a201
Davi Viana <davipviana@gmail.com>
2020-05-21 13:52:46 -0300

    include backdrop in side drawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,21 +1,26 @@
 import React from 'react';
 
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Wrapper from '../../../hoc/Wrapper';
 
 import styles from './SideDrawer.module.css';
 
 const SideDrawer = (props) => {
 
     return (
-        <div className={styles.SideDrawer}>
-            <div className={styles.Logo}>
-                <Logo />
+        <Wrapper>
+            <Backdrop visible />
+            <div className={styles.SideDrawer}>
+                <div className={styles.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Wrapper>
     );
 };
 
