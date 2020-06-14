3358dd0f44dba58a69ae78cf1994f2bf550361f0
Davi Viana <davipviana@gmail.com>
2020-06-14 10:57:08 -0300

    input component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
+import Input from '../../../components/UI/Input/Input';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 
 import styles from './ContactData.module.css';
@@ -49,10 +50,10 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <input className={styles.Input} type="text" name="name" placeholder="Your name" />
-                <input className={styles.Input} type="email" name="email" placeholder="Your email" />
-                <input className={styles.Input} type="text" name="street" placeholder="Street" />
-                <input className={styles.Input} type="text" name="postalCode" placeholder="Postal Code" />
+                <Input inputtype="input" type="text" name="name" placeholder="Your name" />
+                <Input inputtype="input" type="email" name="email" placeholder="Your email" />
+                <Input inputtype="input" type="text" name="street" placeholder="Street" />
+                <Input inputtype="input" type="text" name="postalCode" placeholder="Postal Code" />
                 <Button type="Success" onClick={this.orderHandler}>ORDER</Button>
             </form>
         );
