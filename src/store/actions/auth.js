51d002fe86059574e602d2a1be852285d7555793
Davi Viana <davipviana@gmail.com>
2020-06-20 19:30:50 -0300

    storing the token in auth reducer

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -10,10 +10,11 @@ const authStart = () => {
   }
 };
 
-const authSuccess = (authData) => {
+const authSuccess = (idToken, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData
+    idToken: idToken,
+    userId: userId
   }
 };
 
@@ -41,7 +42,7 @@ export const auth = (email, password, isSignup) => {
     axios.post(url, authData)
       .then(response => {
         console.log(response);
-        dispatch(authSuccess(response.data));
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
       })
       .catch(error => {
         console.log(error);
