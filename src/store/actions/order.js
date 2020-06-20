e2c350cbc3e33c7b9c405be1ebf6524c3dbaa049
Davi Viana <davipviana@gmail.com>
2020-06-20 15:36:07 -0300

    do not export internal actions

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,7 +1,7 @@
 import * as actionTypes from './actionTypes';
 import axios from '../../axios-orders';
 
-export const purchaseBurgerSuccess = (id, orderData) => {
+const purchaseBurgerSuccess = (id, orderData) => {
   return {
     type: actionTypes.PURCHASE_BURGER_SUCCESS,
     orderId: id,
@@ -9,14 +9,14 @@ export const purchaseBurgerSuccess = (id, orderData) => {
   }
 };
 
-export const purchaseBurgerFail = (error) => {
+const purchaseBurgerFail = (error) => {
   return {
     type: actionTypes.PURCHASE_BURGER_FAIL,
     error: error
   };
 };
 
-export const purchaseBurgerStart = () => {
+const purchaseBurgerStart = () => {
   return {
     type: actionTypes.PURCHASE_BURGER_START
   };
