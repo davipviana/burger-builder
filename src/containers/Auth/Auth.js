7a45a7acdcd072fe301558fff77ca9b0ae61c821
Davi Viana <davipviana@gmail.com>
2020-06-20 19:33:43 -0300

    show spinner when is autenticating

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 import * as actions from '../../store/actions';
 
@@ -97,7 +98,7 @@ class Auth extends Component {
       })
     }
 
-    const form = formElementsArray.map(formElement => (
+    let form = formElementsArray.map(formElement => (
       <Input
         key={formElement.id}
         elementType={formElement.config.elementType}
@@ -108,6 +109,10 @@ class Auth extends Component {
         touched={formElement.config.touched}
         onChange={(event) => this.inputChangedHandler(event, formElement.id)} />
     ))
+
+    if (this.props.loading) {
+      form = <Spinner />
+    }
 
     return (
       <div className={styles.Auth}>
@@ -124,10 +129,16 @@ class Auth extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    loading: state.auth.loading
+  }
+}
+
 const mapDispatchToProps = dispatch => {
   return {
     doAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
   }
 }
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
