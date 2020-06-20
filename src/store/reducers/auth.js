6d034850c0f2c2688db71ce9be8d67a981211d7d
Davi Viana <davipviana@gmail.com>
2020-06-20 19:48:57 -0300

    logout user after token expiration

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -23,11 +23,16 @@ const authFail = (state, action) => {
   return updateObject(state, { error: action.error, loading: false });
 }
 
+const authLogout = (state) => {
+  return updateObject(state, { token: null, userId: null });
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START: return authStart(state);
     case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
     case actionTypes.AUTH_FAIL: return authFail(state, action);
+    case actionTypes.AUTH_LOGOUT: return authLogout(state);
     default: return state;
   }
 }
