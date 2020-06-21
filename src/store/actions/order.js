cb5e542342258aa350327f496ace7ca2b0668c7a
Davi Viana <davipviana@gmail.com>
2020-06-21 17:36:31 -0300

    remove console log from order actions file

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -28,7 +28,6 @@ export const purchaseBurger = (order, token) => {
 
     axios.post('/orders.json?auth=' + token, order)
       .then(response => {
-        console.log(response.data)
         dispatch(purchaseBurgerSuccess(response.data.name, order));
       })
       .catch(error => {
