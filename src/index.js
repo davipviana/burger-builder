3fc87e716288f32f36dbd2ffe44efca1d944d659
Davi Viana <davipviana@gmail.com>
2020-07-09 09:22:31 -0300

    remove redux-thunk

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 
 import './index.css';
@@ -31,7 +30,7 @@ const rootReducer = combineReducers({
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(rootReducer, composeEnhancers(
-  applyMiddleware(thunk, sagaMiddleware)
+  applyMiddleware(sagaMiddleware)
 ));
 
 sagaMiddleware.run(watchAuth);
