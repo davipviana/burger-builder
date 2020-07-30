5fc25e8d34b04974ea7906aeecdf1d137733a77d
Davi Viana <davipviana@gmail.com>
2020-07-30 10:03:27 -0300

    fix string concatenation

--- src/store/sagas/order.js
+++ src/store/sagas/order.js
@@ -6,7 +6,7 @@ import * as actions from '../actions';
 export function* purchaseBurgerSaga(action) {
   yield put(actions.purchaseBurgerStart());
   try {
-    const response = yield axiosInstance.post('/orders.json?auth=' + action.token, action.order);
+    const response = yield axiosInstance.post(`/orders.json?auth=${action.token}`, action.order);
     yield put(actions.purchaseBurgerSuccess(response.data.name, action.order));
   } catch (error) {
     yield put(actions.purchaseBurgerFail(error));
@@ -15,11 +15,11 @@ export function* purchaseBurgerSaga(action) {
 
 export function* fetchOrdersSaga(action) {
   yield put(actions.fetchOrdersStart());
-  const queryParams = '?auth=' + action.token + '&orderBy="userId"&equalTo="' + action.userId + '"';
-  const response = yield axiosInstance.get('orders.json' + queryParams);
+  const queryParams = `?auth=${action.token}&orderBy="userId"&equalTo="${action.userId}"`;
+  const response = yield axiosInstance.get(`orders.json${queryParams}`);
   try {
     const orders = [];
-    for (let key in response.data) {
+    for (const key in response.data) {
       orders.push({
         ...response.data[key],
         id: key
