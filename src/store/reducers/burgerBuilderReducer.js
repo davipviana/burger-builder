8df2747bf777bfe949ca8b8b8cb89359ee304038
Davi Viana <davipviana@gmail.com>
2020-07-31 12:17:39 -0300

    round total price

--- src/store/reducers/burgerBuilderReducer.js
+++ src/store/reducers/burgerBuilderReducer.js
@@ -1,70 +1,79 @@
-import * as actionTypes from '../actions/actionTypes';
-import { updateObject } from '../../shared/utils';
+import * as actionTypes from "../actions/actionTypes";
+import { updateObject, roundNumber } from "../../shared/utils";
 
 const initialState = {
-    ingredients: null,
-    totalPrice: 5,
-    error: false,
-    building: false
-}
+  ingredients: null,
+  totalPrice: 5,
+  error: false,
+  building: false,
+};
 
 const INGREDIENT_PRICES = {
-    salad: 0.5,
-    cheese: 0.4,
-    meat: 1.3,
-    bacon: 0.7
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7,
 };
 
 const addIngredient = (state, action) => {
-    return {
-        ...state,
-        ingredients: {
-            ...state.ingredients,
-            [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-        },
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
-        building: true
-    };
-}
+  return {
+    ...state,
+    ingredients: {
+      ...state.ingredients,
+      [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+    },
+    totalPrice: roundNumber(
+      state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    ),
+    building: true,
+  };
+};
 
 const removeIngredient = (state, action) => {
-    return {
-        ...state,
-        ingredients: {
-            ...state.ingredients,
-            [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-        },
-        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
-        building: true
-    };
-}
+  return {
+    ...state,
+    ingredients: {
+      ...state.ingredients,
+      [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+    },
+    totalPrice: roundNumber(
+      state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+    ),
+    building: true,
+  };
+};
 
 const fetchIngredientsSuccess = (state, action) => {
-    return {
-        ...state,
-        ingredients: action.ingredients,
-        totalPrice: 5,
-        error: false,
-        building: false
-    };
-}
+  return {
+    ...state,
+    ingredients: action.ingredients,
+    totalPrice: 5,
+    error: false,
+    building: false,
+  };
+};
 
 const fetchIngredientsFail = (state, action) => {
-    return updateObject(state, { error: true });
-}
+  return updateObject(state, { error: true });
+};
 
 const burgerBuilderReducer = (state, action) => {
-    if (state === undefined || state === null) {
-        return initialState;
-    }
+  if (state === undefined || state === null) {
+    return initialState;
+  }
 
-    switch (action.type) {
-        case actionTypes.ADD_INGREDIENT: return addIngredient(state, action);
-        case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state, action);
-        case actionTypes.FETCH_INGREDIENTS_SUCCESS: return fetchIngredientsSuccess(state, action);
-        case actionTypes.FETCH_INGREDIENTS_FAIL: return fetchIngredientsFail(state, action);
-        default: return state;
-    }
-}
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENT:
+      return addIngredient(state, action);
+    case actionTypes.REMOVE_INGREDIENT:
+      return removeIngredient(state, action);
+    case actionTypes.FETCH_INGREDIENTS_SUCCESS:
+      return fetchIngredientsSuccess(state, action);
+    case actionTypes.FETCH_INGREDIENTS_FAIL:
+      return fetchIngredientsFail(state, action);
+    default:
+      return state;
+  }
+};
 
 export default burgerBuilderReducer;
