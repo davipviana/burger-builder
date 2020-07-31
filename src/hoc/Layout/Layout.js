dce124d6a31a24d68409f840eeeac09a953e3507
Davi Viana <davipviana@gmail.com>
2020-07-31 12:26:02 -0300

    loading

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,44 +1,42 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from "react";
+import { connect } from "react-redux";
 
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
-import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
-import styles from './Layout.module.css';
+import styles from "./Layout.module.css";
 
 const Layout = (props) => {
-    const [showSideDrawer, setShowSideDrawer] = useState(false);
+  const [showSideDrawer, setShowSideDrawer] = useState(false);
 
-    const hideSideDrawerHandler = () => {
-        setShowSideDrawer(false);
-    }
+  const hideSideDrawerHandler = () => {
+    setShowSideDrawer(false);
+  };
 
-    const sideDrawerToggleClickHandler = () => {
-        setShowSideDrawer(prevShowSideDrawer => !prevShowSideDrawer);
-    }
+  const sideDrawerToggleClickHandler = () => {
+    setShowSideDrawer((prevShowSideDrawer) => !prevShowSideDrawer);
+  };
 
+  return (
+    <React.Fragment>
+      <Toolbar
+        isAuthenticated={props.isAuthenticated}
+        onClickDrawerToggle={sideDrawerToggleClickHandler}
+      />
+      <SideDrawer
+        isAuthenticated={props.isAuthenticated}
+        visible={showSideDrawer}
+        onClose={hideSideDrawerHandler}
+      />
+      <main className={styles.Content}>{props.children}</main>
+    </React.Fragment>
+  );
+};
 
-    return (
-        <React.Fragment>
-            <Toolbar
-                isAuthenticated={props.isAuthenticated}
-                onClickDrawerToggle={sideDrawerToggleClickHandler} />
-            <SideDrawer
-                isAuthenticated={props.isAuthenticated}
-                visible={showSideDrawer}
-                onClose={hideSideDrawerHandler} />
-            <div>SideDrawer, Backdrop</div>
-            <main className={styles.Content}>
-                {props.children}
-            </main>
-        </React.Fragment>
-    );
-}
-
-const mapStateToProps = state => {
-    return {
-        isAuthenticated: state.auth.token !== null
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  };
+};
 
 export default connect(mapStateToProps)(Layout);
