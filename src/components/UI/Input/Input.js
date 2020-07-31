3145f8e42884eac498fed4be6c3ebae80374e4a1
Davi Viana <davipviana@gmail.com>
2020-07-31 11:42:05 -0300

    auth internationalization

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,57 +1,66 @@
-import React from 'react';
+import React from "react";
+import { useTranslation } from "react-i18next";
 
-import styles from './Input.module.css';
+import styles from "./Input.module.css";
 
 const Input = (props) => {
-    let inputElement = null;
-    let validationError = null;
-    const inputStyles = [styles.InputElement];
+  const { t } = useTranslation();
+  let inputElement = null;
+  let validationError = null;
+  const inputStyles = [styles.InputElement];
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inputStyles.push(styles.Invalid);
+    validationError = <p>Please enter a valid value!</p>;
+  }
 
-    if (props.invalid && props.shouldValidate && props.touched) {
-        inputStyles.push(styles.Invalid);
-        validationError = <p>Please enter a valid value!</p>;
-    }
+  switch (props.elementType) {
+    case "input":
+      inputElement = (
+        <input
+          className={inputStyles.join(" ")}
+          {...props.elementConfig}
+          placeholder={t(props.elementConfig.placeholder)}
+          value={props.value}
+          onChange={props.onChange}
+        />
+      );
+      break;
+    case "textarea":
+      inputElement = (
+        <textarea
+          className={inputStyles.join(" ")}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.onChange}
+        />
+      );
+      break;
+    case "select":
+      inputElement = (
+        <select
+          className={inputStyles.join(" ")}
+          value={props.value}
+          onChange={props.onChange}
+        >
+          {props.elementConfig.options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+          ))}
+        </select>
+      );
+      break;
+    default:
+      throw new Error("Unsupported input element type");
+  }
 
-    switch (props.elementType) {
-        case ('input'):
-            inputElement = <input
-                className={inputStyles.join(' ')}
-                {...props.elementConfig}
-                value={props.value}
-                onChange={props.onChange} />;
-            break;
-        case ('textarea'):
-            inputElement = <textarea
-                className={inputStyles.join(' ')}
-                {...props.elementConfig}
-                value={props.value}
-                onChange={props.onChange} />;
-            break;
-        case ('select'):
-            inputElement = (
-                <select
-                    className={inputStyles.join(' ')}
-                    value={props.value}
-                    onChange={props.onChange}>
-                    {props.elementConfig.options.map(option => (
-                        <option key={option.value} value={option.value}>
-                            {option.displayValue}
-                        </option>
-                    ))}
-                </select>
-            );
-            break;
-        default:
-            throw new Error('Unsupported input element type');
-    }
-
-    return (
-        <div className={styles.Input}>
-            <label className={styles.Label}>{props.label}</label>
-            {inputElement}
-            {validationError}
-        </div>
-    );
+  return (
+    <div className={styles.Input}>
+      <label className={styles.Label}>{props.label}</label>
+      {inputElement}
+      {validationError}
+    </div>
+  );
 };
 
 export default Input;
